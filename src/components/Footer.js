cb410988345379ba8cdd13326800dc2b8dd83c5f
Pof Magicfingers <pof@podshows.fr>
2019-09-22 12:09:55 +0200

    feat: add color palette

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,23 +3,23 @@ import { Link } from "gatsby";
 
 import logo from "../img/logo.svg";
 import facebook from "../img/social/facebook.svg";
+import github from "../img/social/github.svg";
 import instagram from "../img/social/instagram.svg";
 import twitter from "../img/social/twitter.svg";
-import vimeo from "../img/social/vimeo.svg";
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
+      <footer className="footer has-background-secondary has-text-white-ter">
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="gicma.dev"
             style={{ width: "14em", height: "10em" }}
           />
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
+        <div className="content has-text-centered has-background-secondary has-text-white-ter">
+          <div className="container has-background-secondary has-text-white-ter">
             <div className="columns">
               <div className="column is-4">
                 <section className="menu">
@@ -96,10 +96,10 @@ const Footer = class extends React.Component {
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
-                <a title="vimeo" href="https://vimeo.com">
+                <a title="github" href="https://github.com/gicmadev">
                   <img
-                    src={vimeo}
-                    alt="Vimeo"
+                    src={github}
+                    alt="GitHub"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
